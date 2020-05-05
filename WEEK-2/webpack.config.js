193ac5803dc836b93b6a29470c3985d3e7b5106f
Aditya_kumawat <31047132+AdityaKumawat97@users.noreply.github.com>
2020-05-05 18:32:41 +0530

    week-2 assignment initialted
    week-2

---
+++ WEEK-2/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.scss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ],
+            },
+        ],
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'dist/index.html'
+        })
+    ],
+
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 3000
+    }
+
+};
